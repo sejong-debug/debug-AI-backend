983fe976c7fa557bac1351f34637b442561e2b6d
JeongJun <80030558+GalaxyOverMe@users.noreply.github.com>
2022-05-25 14:43:27 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ app = Flask(__name__)
 
 @app.route('/detect-issue', methods=['POST'])
 def detectIssues():
+    board_image_id = request.form.get('boardImageId')
     image = request.files['image']
     result = detectIssue(image)
 
-    return {'issues': result}
+    return {'boardImageId': board_image_id, 'issues': result}
